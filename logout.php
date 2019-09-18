fb8690533ad54386e03368f65a2e5fd71d37a190
zamzammmm <zamzamzuba2017@gmail.com>
2019-09-18 14:04:22 +0700

    form Array "Login"

---
+++ logout.php
@@ -0,0 +1,8 @@
+<?php
+    session_start();
+    if (isset($_SESSION['Login'])) {
+    unset ($_SESSION);
+    session_destroy();
+    echo "<h1>Anda sudah berhasil LOGOUT</h1>";
+    echo "<h2>Klik <a href='login.php'>di sini</a> untuk LOGIN kembali</h2>";
+}
